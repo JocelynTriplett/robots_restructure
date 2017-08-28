92c89ec1f03c87d37950da7dd6f3c2518664adf5
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-28 18:53:22 -0400

    finds all employed robots

--- app.js
+++ app.js
@@ -27,6 +27,16 @@ app.use('/unemployed', function (req, res) {
   })
 })
 
+app.use('/employed', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    robots.find({job: {$ne: null}}).toArray(function(err, docs){
+      console.log(docs);
+      res.render('unemployed', {robots: docs})
+    })
+  })
+})
+
 // https://nicksardo.wordpress.com/2015/11/26/injecting-javascript-variables-into-mongodb-queries-in-meteor/
 app.use('/:dynamic', function (req, res) {
   MongoClient.connect(mongoURL, function (err, db) {
